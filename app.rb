91ae64fefdb01b71baa842ab69ed60f009163449
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-06 15:59:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -16,9 +16,11 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @phrase = params[:phrase]
     @number = params[:number].to_i
+    answer = ""
     @number.times do 
-      "#{@phrase}"
-    end 
+      answer += @phrase
+    end
+    answer
   end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
@@ -27,5 +29,19 @@ class App < Sinatra::Base
   end 
 
   get '/:operation/:number1/:number2' do 
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    case @operation
+    when "add"
+      @answer = @number1 + @number2
+    when "subtract"
+      @answer = @number1 - @number2
+    when "multiply"
+      @answer = @number1 * @number2
+    when "divide"
+      @answer = @number1 / @number2
+    end
+    erb :operation
   end 
 end
